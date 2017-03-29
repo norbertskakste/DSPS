31444d1e3fe6978b934c8783f8c94841c5ee227d
Norberts Kakste <norberts.kakste@autentica.lv>
2017-03-29 17:47:39 +0300

    Rewriting session model

--- web/models/session.ex
+++ web/models/session.ex
@@ -17,8 +17,7 @@ defmodule Dsps.Session do
     end
 
     def current_user(conn) do
-        id = Plug.Conn.get_session(conn, :current_user)
-        if id, do: Dsps.Repo.get(User, id)
+        
     end
 
     def logged_in?(conn), do: !!current_user(conn)
