d465284affab73dd9dc881fd63e76e66b0412d5e
Norberts Kakste <norberts.kakste@autentica.lv>
2017-04-06 17:24:25 +0300

    Fixed UserTest

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule Dsps.UserTest do
 
   alias Dsps.User
 
-  @valid_attrs %{email: "some content", password: "some content", username: "some content"}
+  @valid_attrs %{email: "email@email.com", password: "securePassword", username: "niceUsername"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
