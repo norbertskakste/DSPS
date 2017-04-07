b3776625f78e6b42399d7d33b6f259eee09a73ca
Norberts Kakste <norberts.kakste@autentica.lv>
2017-04-07 23:42:46 +0300

    Fixed travis Env

--- config/travis.exs
+++ config/travis.exs
@@ -1,25 +1,13 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
 config :dsps, Dsps.Endpoint,
-  http: [port: 4000],
-  debug_errors: true,
-  code_reloader: true,
-  check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../", __DIR__)]]
+  http: [port: 4001],
+  server: false
 
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
-
-# Set a higher stacktrace during development. Avoid configuring such
-# in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 30
+# Print only warnings and errors during test
+config :logger, level: :warn
 
 # Configure your database
 config :dsps, Dsps.Repo,
