df0f067032411402248b978b6a0effc598c9b274
Norberts Kakste <norberts.kakste@autentica.lv>
2017-04-08 23:43:09 +0300

    fixed redis session test

--- test/redis/session_test.exs
+++ test/redis/session_test.exs
@@ -1,10 +1,11 @@
 defmodule Dsps.RedisTest.Session do
     import Dsps.Redis.Session
+
     use ExUnit.Case
 
     test "redis session creation" do
         user = Dsps.Redis.Session.UserSession.new_session(1)
-        Dsps.Redis.Session.new_redis_session("testUUID")
+        Dsps.Redis.Session.new_redis_session("testUUID", user)
 
         assert Dsps.Redis.Session.get_redis_session("testUUID") != :undefined
     end
