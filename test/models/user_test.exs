69197d41c6650b9883636d294f51624aaf8a307a
Norberts Kakste <norberts.kakste@autentica.lv>
2017-04-08 13:34:16 +0300

    Invalid user model tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -4,9 +4,21 @@ defmodule Dsps.UserTest do
     alias Dsps.User
 
     @valid_attrs %{email: "test@test.com", username: "testUsername", virtual_password: "testPassword", password: "testPassword"}
+    @invalid_email_attrs %{email: "email", username: "testUsername", virtual_password: "testPassword", password: "testPassword" }
+    @invalid_attrs%{email: "test@test.com", username: "testUsername"}
 
     test "changeset with valid attributes" do
         changeset = User.changeset(%User{}, @valid_attrs)
         assert changeset.valid?
+    end
+
+    test "changeset with invalid email" do
+        changeset = User.changeset(%User{}, @invalid_email_attrs)
+        assert !changeset.valid?
+    end
+
+    test "changeset with invalid attributes" do
+        changeset = User.changeset(%User{}, @invalid_attrs)
+        assert !changeset.valid?
     end
 end
