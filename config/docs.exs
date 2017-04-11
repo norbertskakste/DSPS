969037233a756130dc74f18c0fd3b00cbc6dc5d9
Norberts Kakste <norberts.kakste@autentica.lv>
2017-04-11 13:44:07 +0300

    InchCI fix

---
+++ config/docs.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :dsps, Dsps.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :dsps, Dsps.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  username: "postgres",
+  password: "",
+  database: "dsps_test",
+  hostname: "localhost"
